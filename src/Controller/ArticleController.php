f3e0b6cde0466c6d91b11129af2876a4b8af1104
AlexanderAbdulla <alexander.amadeus.abdulla@gmail.com>
2018-11-18 21:17:59 -0800

    Symfony Practice
    :)

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,9 +2,20 @@
     namespace App\Controller;
 
     use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-    class ArticleController {
+    class ArticleController extends Controller {
+        /** 
+        * @Route("/")
+        * @Method({"GET"})
+        */
         public function index(){
-            return new Response('<html<body>Hello</body></html>');
+
+            $articles = ['Article 1', 'Article 2'];
+
+            //return new Response('<html<body>Hello</body></html>');
+            return $this->render('articles/index.html.twig', array('articles' => $articles));
         }
     }
