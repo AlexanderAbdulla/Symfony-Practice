2bedf5f717fed927b5b010a6c2ee73c9005df5a6
AlexanderAbdulla <alexander.amadeus.abdulla@gmail.com>
2018-11-20 16:52:20 -0800

    Fixed Delete Bug

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,15 @@
 const articles = document.getElementById('articles');
 
-if(articles) {
-    articles.addEventListener('click', e => {
-        //alert(2);
+if (articles) {
+  articles.addEventListener('click', e => {
+    if (e.target.className === 'btn btn-danger delete-article') {
+      if (confirm('Are you sure?')) {
+        const id = e.target.getAttribute('data-id');
 
-        if(e.target.className === 'btn btn-danger delete-article'){
-            if(confirm('Are you sure?')){
-                const id = e.target.getAttribute('data-id');
-                
-                fetch('/article/delete/${id}', {
-                    method: 'DELETE'
-                }).then(res => window.location.reload());
-            }
-        }
-    });
+        fetch(`/article/delete/${id}`, {
+          method: 'DELETE'
+        }).then(res => window.location.reload());
+      }
+    }
+  });
 }
